bfa2a955cfc4f649e84db8ec4e23515cde70d04c
ChrisRoyalty <chrisroyalty001@gmail.com>
2023-08-31 15:55:18 +0100

    I just cloned a landing page-SYBERCAMP

---
+++ app.js
@@ -0,0 +1,9 @@
+let menu = document.getElementsByClassName("menu");
+let menuDropdown = document.getElementById("menuDropdown");
+
+function showDropdown() {
+  menuDropdown.style.display = "flex";
+}
+function cancelDropdown() {
+  menuDropdown.style.display = "none";
+}
